60aa5cbc15502282b85e7f930618858f9989868e
GogetaBlueMUI <muhammadabdullah090809@gmail.com>
2026-03-31 15:34:06 +0500

    modified 8 program

--- Flask_Practice_8/form.py
+++ Flask_Practice_8/form.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
 class RegistrationForm(FlaskForm):
-    name=StringField("Full Name", validators=[DataRequired()])
-    email=StringField("Email",validators=[DataRequired(),Email()])
-    password=PasswordField("Password",validators=[DataRequired(),Length(min=8)])
+    name=StringField("Full Name", validators=[DataRequired(message="Name is Required")])
+    email=StringField("Email",validators=[DataRequired(message="Email is Required"),Email(message="Not a Valid Email")])
+    password=PasswordField("Password",validators=[DataRequired("Password is Required"),Length(min=8, message="Password should be 8 Characters Long")])
     submit=SubmitField("Register")
